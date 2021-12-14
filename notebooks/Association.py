e5982b92571a892f9440885307be167cff8cb338
Toon Meynen <toonmey@gmail.com>
2021-12-14 13:55:09 +0100

    update

--- notebooks/Association.py
+++ notebooks/Association.py
@@ -1,14 +1,13 @@
 class Association:
-    def __init__(self, l, r, c, s):
+    def __init__(self, l, r, c, s, lift, z):
         self.left = l
         self.right = r
         self.c = c
-        self.C = c
         self.s = s
-        self.S = s
-        self.score = 0
+        self.lift = lift
+        self.z = z
         
     def __str__(self):
-        return "Conf: {:.2f}\tSupp: {:.2f}\t".format(self.C, self.S) + \
+        return "Conf: {:.2f}\tSupp: {:.2f}\tLift: {:.5f}Z: {:.5f}\n\t".format(self.c, self.s, self.lift, self.z) + \
             " {" + ", ".join(map(str, self.left)) + \
             "} => {" + ", ".join(map(str, self.right)) + "}"
